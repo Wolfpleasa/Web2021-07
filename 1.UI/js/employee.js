653dc8471328986916315afc98376191c800fbe3
Wolfpleasa <dominhngoc2611@gmail.com>
2021-07-17 17:39:17 +0700

    update

--- 1.UI/js/employee.js
+++ 1.UI/js/employee.js
@@ -1,21 +1,34 @@
+/**
+ * Event hiện pop up 
+ * Ngọc 16/7/2021
+ */
 $(".content-header .button").click(function() {
     $(".popup").css('visibility', 'visible');
     $("#focus").focus();
     $(this).attr("disable", 'true');
 });
 
-$(".head-close").click(function() {
+/**
+ * Event ẩn pop up 
+ * Ngọc 16/7/2021
+ */
+$(".head-close, .button.cancel").click(function() {
     $(".popup").css('visibility', 'hidden');
+    $(".X").attr("style", "visibility: hidden;")
 })
 
-$(".button.cancel").click(function() {
-    $(".popup").css('visibility', 'hidden');
-})
-
+/**
+ * reload lại trang
+ * Ngọc 16/7/2021
+ */
 $(".refresh").click(function() {
     location.reload();
 })
 
+/**
+ * Load ảnh từ máy lên form thêm mới
+ * Ngọc 16/7/2021
+ */
 $('.image').click(function() {
     $('#myFile').trigger('click');
 })
@@ -25,36 +38,4 @@ $('#myFile').click(function(e) {
         var img = URL.createObjectURL(e.target.files[0]);
         $('.image').css("background-image", `url(${img})`);
     })
-})
-
-//dropdown
-$(".select-arrow").click(function() {
-    let $item = $(this)
-
-    if ($item.hasClass("down")) {
-        $item.removeClass("down");
-        $(this).parent().parent().find(".dropdown").attr("style", "display: block;");
-    } else {
-        $item.addClass("down");
-        $(this).parent().parent().find(".dropdown").attr("style", "display: none;");
-    }
-})
-$(".dropdown-item").click(function() {
-    console.log("item");
-    let val = $(this).find(".dropdown-text").text();
-    console.log(val);
-    $(this).parents('.dropdown').find('.dropdown-item').removeClass("bg-select");
-    $(this).addClass("bg-select");
-    $(this).parent().parent().find(".inp").text(val);
-    $(this).parent().parent().find(".X").attr("style", "visibility: visible;");
-})
-
-$(document).on("click", function(e) {
-    let $el = $(e.target),
-        isArrow = $el.data("select-arrow");
-
-    if (!isArrow) {
-        $(".dropdown").hide();
-        // $(".content-control .arrow").removeClass("down");
-    }
 })
